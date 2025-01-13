5304643f275943ac40516bf1ae00ffae5346e72e
Bartłomiej Kaftan <barkaf@proton.me>
2025-01-13 09:42:41 +0100

    add scalar

--- RestaurantApi/Program.cs
+++ RestaurantApi/Program.cs
@@ -5,6 +5,7 @@ using RestaurantApi.Entities;
 using RestaurantApi.Mapper;
 using RestaurantApi.Middleware;
 using RestaurantApi.Services;
+using Scalar.AspNetCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 LogManager.Setup().LoadConfigurationFromAppSettings();
@@ -13,12 +14,27 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<RestaurantDbContext>();
 builder.Services.AddAutoMapper(typeof(RestaurantMappingProfile).Assembly);
 builder.Services.AddScoped<IRestaurantService, RestaurantService>();
+builder.Services.AddOpenApi();
 
 // U¿yj domyœlnego logowania
 builder.Logging.ClearProviders(); // Opcjonalnie, aby wyczyœciæ inne dostawców logów
 builder.Host.UseNLog(); // U¿yj NLog jako loggera
 
 WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    // https://localhost:7284/scalar/v1
+    app.MapScalarApiReference(options =>
+    {
+        options
+        .WithTitle("Restaurant API")
+        .WithTheme(ScalarTheme.Mars)
+        .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient)
+        .WithDarkMode(true);
+    });
+}
 
 app.UseMiddleware<ErrorHandlingMiddleware>();
 
