f8280bb06ca9e9a74b1b54ed9afc4763141f27ba
Bartłomiej Kaftan <barkaf@proton.me>
2025-01-20 02:24:15 +0100

    add test project

---
+++ RestaurantApi.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantApi.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
